1d425259ef9cf522c323daddda5e2ce5c2af6356
darylaw93 <darylaw93@gmail.com>
2021-09-30 19:28:51 +0800

    api testing

--- real_estate/src/components/HomePage.js
+++ real_estate/src/components/HomePage.js
@@ -1,13 +1,58 @@
-import React from "react"
-import NavBar from "./NavBar"
+import React, { useRef, useState } from "react";
+import NavBar from "./NavBar";
+import axios from "axios";
 
 const HomePage = () => {
+const [block, setBlock] = useState("")
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-    return (
-        <NavBar>
-            <h1>Hi</h1>
-        </NavBar>
-    )
-}
+  const address = useRef(null);
+  const handleQuery = () => {
+    console.log(address.current.value)
+    // axios
+    //   .get(
+    //     `https://data.gov.sg/api/action/datastore_search?resource_id=482bfa14-2977-4035-9c61-c85f871daf4e&q=${address.current.value}`
+    //   )
+    //   .then(function (res) {
+    //     console.log(res.data.result.records);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   })
+    //   .then(function () {});
 
-export default HomePage
+    axios
+      .get(
+        `https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&q=${address.current.value}`
+      )
+      .then(function (res) {
+        console.log(res.data.result.records);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {});
+
+  };
+  return (
+    <NavBar>
+      <div>
+        <h1>Properties Search</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="search"
+            ref={address}
+            onChange={handleQuery}
+            required
+          ></input>
+          <button>Search</button>
+        </form>
+      </div>
+    </NavBar>
+  );
+};
+
+export default HomePage;
--- real_estate/src/components/NavBar.js
+++ real_estate/src/components/NavBar.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = (props) => {
   return (
     <>
-      <div className="Nav">Navbar Works!</div>
-      {props.children}
+      <div className="Nav">
+        <span className="NavContent">
+          <Link to="/">Home</Link>
+        </span>
+        <span className="NavContent">
+          <Link to="/about">About</Link>
+        </span>
+      </div>
+      <div>{props.children}</div>
     </>
   );
 };
